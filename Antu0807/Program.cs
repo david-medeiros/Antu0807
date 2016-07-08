d225cff9872779ab26b2e4cb279d37b8b731fa12
DavidMedeiros <ronald.junior@cejoseleitelopes.com.br>
2016-07-08 10:29:41 -0300

    Calcular raiz quadradas pela subtração
    Maneira de calcular a raiz quadrada de números subtraindo uma sequencia
    de numeros ímpares a partir do 1.

---
+++ Antu0807/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anto0807
+{
+    class Program
+    {
+        static int p;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número para achar sua raiz quadrada");
+            p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(sqrt(p));
+            Console.ReadKey();
+        }
+        static int sqrt(int n)
+        {
+            int i = 1;
+            int c = 1;
+            int dif = n - i;
+            while (dif != 0)
+            {
+                n -= i;
+                i += 2;
+                dif = n - i;
+                c++;
+            }
+            return c;
+        }
+    }
+}
